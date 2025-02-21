eb18b080fae1b3d75c069b19c3c7dde10f3968b7
arang129 <48246455+arang129@users.noreply.github.com>
2025-02-21 11:30:33 +0800

    Update aita.py

--- aita.py
+++ aita.py
@@ -7,7 +7,7 @@ from copy import copy
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-__version__ = '0.01'
+__version__ = '0.02'
 
 # This is the entry point for jupyter-server-proxy . The packaging metadata
 # tells it about this function. For details, see:
@@ -68,7 +68,7 @@ TEMPLATE = """\
 <!DOCTYPE html>
 <html>
 <head>
-	<title>AI助教</title>
+	<title>Yunlab AI</title>
 </head>
  
 <body>
